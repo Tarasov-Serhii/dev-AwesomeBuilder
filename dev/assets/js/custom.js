de75f05a78b88ac88e94ecd4a6f583765c181876
Сергей Тарасов <tsa.21.1999@gmail.com>
2024-10-22 15:55:09 +0300

    floor-plan

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -15,11 +15,55 @@ const buildItem = document.querySelectorAll('.build-item')
             const buildFlatsAction = build.getAttribute('data-flats-action')
             const buildFlatsReservation = build.getAttribute('data-flats-reservation')
 
+            if (Number(buildFlatsFree) === 0) {
+                build.classList.add('build-sold')
+            }
+        
+            build.addEventListener('click', function(event) {
+                console.log('click');
+                
+                if (build.classList.contains('build-sold')) {
+                    event.preventDefault();
+                }
+            })
+
             informationAddress.innerHTML = buildAddress
             informationFloors.innerHTML = buildFloors
             informationFlatsAll.innerHTML = buildFlatsAll
             informationFlatsFree.innerHTML = buildFlatsFree
             informationFlatsAction.innerHTML = buildFlatsAction
             informationFlatsReservation.innerHTML = buildFlatsReservation
+        })
+    })
+
+
+
+
+
+    const floorItem = document.querySelectorAll('.floor-item')
+     const FloorinformationFlatsFree = document.querySelector('#floor-information-flats-free')
+     const FloorinformationFlatsAction = document.querySelector('#floor-information-flats-action')
+     const FloorinformationFlatsReservation = document.querySelector('#floor-information-flats-reservation')
+     floorItem.forEach(floor => {
+        floor.addEventListener('mouseover', function() {
+            const floorFlatsFree = floor.getAttribute('data-floor-flats-free')
+            const floorFlatsAction = floor.getAttribute('data-floor-flats-action')
+            const floorFlatsReservation = floor.getAttribute('data-floor-flats-reservation')
+
+            if (Number(floorFlatsFree) === 0) {
+                floor.classList.add('floor-sold')
+            }
+        
+            floor.addEventListener('click', function(event) {
+                console.log('click');
+                
+                if (floor.classList.contains('floor-sold')) {
+                    event.preventDefault();
+                }
+            })
+
+            FloorinformationFlatsFree.innerHTML = floorFlatsFree
+            FloorinformationFlatsAction.innerHTML = floorFlatsAction
+            FloorinformationFlatsReservation.innerHTML = floorFlatsReservation
         })
     })
